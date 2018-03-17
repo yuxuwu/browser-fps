f8309cdbf00a118dbddad105d28e47981fc6b3ad
yuxuwu <ywu148@binghamton.edu>
2018-03-17 18:54:03 -0400

    Updated raycasting to be cleaner

---
+++ globals.js
@@ -0,0 +1,28 @@
+var Map = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,2,0,0,0,0,2,2,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
+
+/* Map width and height */
+var MapWidth = 0;
+var MapHeight = 0;
+
+var MiniMapScale = 8;
+
+/* Affects resolution of images */
+var ScreenWidth = 320;
+var ScreenHeight = 240;
+var StripWidth = 4;
+var NumStrips = Math.ceil(ScreenWidth/StripWidth);
+var FOV = 60 * Math.PI / 180;
+
+/* Distance from player to screen */
+var ViewDist = ((ScreenWidth/2) / Math.tan(FOV/2));
+
+/* Strips Container */
+var strips = [];
